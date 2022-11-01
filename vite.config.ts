e6fcde31fc4164deb8a107f6c2d954ec5476579b
precious_m02uku <premozmtmn@gmail.com>
2022-11-01 18:05:16 +0900

    20221101

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  // 本番時はgithubリポジトリをルートパスにする
+  base: (process.env.NODE_ENV === 'production')
+    ? '/TwitterID-generateLink/' : './',
+  build: {
+    outDir: 'docs'
+  },
+  plugins: [vue()]
 })
